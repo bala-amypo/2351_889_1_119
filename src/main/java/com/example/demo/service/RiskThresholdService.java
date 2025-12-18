e122b219ceb539495c23e98d4ff25f34a9a68636
bala-amypo <bala@amypo.in>
2025-12-18 15:10:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RiskThresholdService.java
@@ -0,0 +1,11 @@
+import java.util.*;
+package com.example.demo.service;
+package com.example.demo.model.RiskThreshold;
+
+public interface RiskThresholdService extends RiskThreshold
+{
+    RiskThreshold createThreshold(RiskThreshold threshold);
+    RiskThreshold update
+
+
+}
