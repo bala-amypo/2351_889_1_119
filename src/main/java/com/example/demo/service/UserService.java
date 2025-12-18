f227f1d3f978c3add4891eb7401a56abb414d2f7
bala-amypo <bala@amypo.in>
2025-12-18 18:26:28 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,21 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
-    User registerUser(User user);
+    private final UserRepository userRepository;
 
-    User getUserByEmail(String email);
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
--- src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,10 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
