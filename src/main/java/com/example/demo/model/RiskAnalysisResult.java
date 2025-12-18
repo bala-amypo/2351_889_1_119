f317330b69516c57beaf5be60eda2160546e4083
bala-amypo <bala@amypo.in>
2025-12-18 09:23:42 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskAnalysisResult.java
+++ src/main/java/com/example/demo/model/RiskAnalysisResult.java
@@ -10,5 +10,52 @@ public class RiskAnalysisResult{
     private Double highestSectorPercentage;
     private boolean isHighRisk;
     private String notes;
+    public RiskAnalysisResult(LocalDate analysis, Double highestSectorPercentage, Double highestStockPercentage, Long id, boolean isHighRisk, String notes) {
+        this.analysis = analysis;
+        this.highestSectorPercentage = highestSectorPercentage;
+        this.highestStockPercentage = highestStockPercentage;
+        this.id = id;
+        this.isHighRisk = isHighRisk;
+        this.notes = notes;
+    }
+
+    public RiskAnalysisResult() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDate getAnalysis() {
+        return analysis;
+    }
+    public void setAnalysis(LocalDate analysis) {
+        this.analysis = analysis;
+    }
+    public Double getHighestStockPercentage() {
+        return highestStockPercentage;
+    }
+    public void setHighestStockPercentage(Double highestStockPercentage) {
+        this.highestStockPercentage = highestStockPercentage;
+    }
+    public Double getHighestSectorPercentage() {
+        return highestSectorPercentage;
+    }
+    public void setHighestSectorPercentage(Double highestSectorPercentage) {
+        this.highestSectorPercentage = highestSectorPercentage;
+    }
+    public boolean isHighRisk() {
+        return isHighRisk;
+    }
+    public void setHighRisk(boolean isHighRisk) {
+        this.isHighRisk = isHighRisk;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -10,5 +10,55 @@ public class User{
     private String email;
     private String password;
     private LocalDateTime createdAt;
+    public User(Long id, String fullName, String email, String password, LocalDateTime createdAt) {
+        this.id = id;
+        FullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
 }
