12643b64aaf42c2a171b9fb628c6b3fa9704604f
bala-amypo <bala@amypo.in>
2025-12-18 10:41:00 +0000

    User commit

--- src/main/java/com/example/demo/service/PortfolioHoldingService.java
+++ src/main/java/com/example/demo/service/PortfolioHoldingService.java
@@ -4,7 +4,11 @@ package com.example.demo.model.PortfolioHolding;
 
 public interface PortfolioHoldingService extends PortfolioHolding
 {
-    PortfolioHolding create
+    PortfolioHolding createHolding(PortfolioHolding holding);
+    PortfolioHolding updateHolding(Long id,PortfolioHolding holding);
+    PortfolioHolding getHoldingById(Long id);
+    PortfolioHolding getHoldingsByPortfolio(Long portfoliold);
+    void deleteHolding (Long id);
 
 
 }
