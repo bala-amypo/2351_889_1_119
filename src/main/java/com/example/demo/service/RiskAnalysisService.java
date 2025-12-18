c148a3b41b0523c9c19974e4d3e0bd0fc786a3fa
bala-amypo <bala@amypo.in>
2025-12-18 15:13:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RiskAnalysisService.java
@@ -0,0 +1,10 @@
+import java.util.*;
+package com.example.demo.service;
+package com.example.demo.model.RiskAnalysisResult;
+
+public interface RiskAnalysisResultService extends RiskAnalysisResult
+{
+   RiskAnalysisResult analyzePortfolio(Long port)
+
+
+}
--- src/main/java/com/example/demo/service/RiskThresholdService.java
+++ src/main/java/com/example/demo/service/RiskThresholdService.java
@@ -5,7 +5,10 @@ package com.example.demo.model.RiskThreshold;
 public interface RiskThresholdService extends RiskThreshold
 {
     RiskThreshold createThreshold(RiskThreshold threshold);
-    RiskThreshold update
+    RiskThreshold updateThreshold(Long id,RiskThreshold threshold);
+    RiskThreshold getActiveThreshold();
+    RiskThreshold getThresholdById(Long id);
+    List<RiskThreshold>getAllThresholds();
 
 
 }
