616261ef21b75130750862ec4218f6c0aa88d717
bala-amypo <bala@amypo.in>
2025-12-18 09:08:42 +0000

    User commit

--- src/main/java/com/example/demo/model/PortfolioHolding.java
+++ src/main/java/com/example/demo/model/PortfolioHolding.java
@@ -3,8 +3,35 @@ import jakarta.persistence.*;
 public class PortfolioHolding{
     @Id
     private Long id;
+    @Positive
     private Double quantity;
+    @
     private Decimal marketValue;
     private localdate lastUpdated;
+    public PortfolioHolding(Long id, Double quantity, Decimal marketValue) {
+        this.id = id;
+        this.quantity = quantity;
+        this.marketValue = marketValue;
+    }
+    public PortfolioHolding() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+    public Decimal getMarketValue() {
+        return marketValue;
+    }
+    public void setMarketValue(Decimal marketValue) {
+        this.marketValue = marketValue;
+    }
 
 }
