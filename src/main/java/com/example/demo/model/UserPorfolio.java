0a1015dfbb2a3cdc946c5a8f28fa74fa17595d29
bala-amypo <bala@amypo.in>
2025-12-18 08:38:42 +0000

    User commit

--- src/main/java/com/example/demo/model/UserPorfolio.java
+++ src/main/java/com/example/demo/model/UserPorfolio.java
@@ -1,9 +1,10 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
 public class UserPortfolio{
-    
+
     private Long id;
     private Long userId;
     private String portfolio;
     private boolean active;
+    
 }
