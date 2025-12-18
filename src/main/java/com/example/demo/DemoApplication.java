2dd0c24b5e2b056cd6b3913b4a09db4f85d8a2c8
bala-amypo <bala@amypo.in>
2025-12-18 18:41:29 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,5 @@
 package com.example.demo;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class DemoApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(DemoApplication.class, args);
-// 	}
-
-// }
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -23,4 +10,3 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
-
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,68 +1,17 @@
 package com.example.demo.model;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
 
-public class User{
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String FullName;
-    @Email(message="invalid")
-    @Column(unique=true)
+
     private String email;
     private String password;
-    private LocalDateTime createdAt;
-    public User(Long id, String fullName, String email, String password, LocalDateTime createdAt) {
-        this.id = id;
-        FullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.createdAt = createdAt;
-    }
 
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return FullName;
-    }
-
-    public void setFullName(String fullName) {
-        FullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
+    // getters & setters
 }
