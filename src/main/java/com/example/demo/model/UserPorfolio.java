d9e85e1d60020070bb46fd54fd60d49b35727d8e
bala-amypo <bala@amypo.in>
2025-12-18 08:43:42 +0000

    User commit

--- src/main/java/com/example/demo/model/UserPorfolio.java
+++ src/main/java/com/example/demo/model/UserPorfolio.java
@@ -1,10 +1,72 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
 public class UserPortfolio{
-
+    @Id
     private Long id;
     private Long userId;
     private String portfolio;
     private boolean active;
-    
+    private localdate Timestamp;
+    private localdate updatedAt;
+    public UserPortfolio(Long id, Long userId, String portfolio, boolean active, localdate timestamp,
+            localdate updatedAt) {
+        this.id = id;
+        this.userId = userId;
+        this.portfolio = portfolio;
+        this.active = active;
+        Timestamp = timestamp;
+        this.updatedAt = updatedAt;
+    }
+
+    public UserPortfolio() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(String portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public localdate getTimestamp() {
+        return Timestamp;
+    }
+
+    public void setTimestamp(localdate Timestamp) {
+        this.Timestamp = Timestamp;
+    }
+
+    public localdate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(localdate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
