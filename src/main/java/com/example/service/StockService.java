f9e7e2ad56fa2b840ca4f45697016752b552201a
bala-amypo <bala@amypo.in>
2025-12-18 06:48:44 +0000

    User commit

--- src/main/java/com/example/service/StockService.java
+++ src/main/java/com/example/service/StockService.java
@@ -1,6 +1,12 @@
 import java.util.*;
+package com.example.demo.service;
+package com.example.demo.entity.Stockentity;
+
 public interface StockService{
     Stockentity savedata(Stockentity st);
     Stockentity getidval(int id);
-    
+    List<Stockentity>getall();
+    Stockentity update(int id,Stockentity st);
+    void delete(int id);
+
 }
