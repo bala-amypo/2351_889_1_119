5dc97128316ad6cc4abf8cf5b52fdceaa2dbd4ed
bala-amypo <bala@amypo.in>
2025-12-18 10:32:22 +0000

    User commit

--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -2,11 +2,13 @@ import java.util.*;
 package com.example.demo.service;
 package com.example.demo.model.Stock;
 
-public interface StockService extends Stock{
-    Stock savedata(Stock st);
-    Stock getidval(int id);
-    List<Stock>getall();
-    Stock update(int id,Stock st);
-    void delete(int id);
+public interface StockService extends Stock
+{
+    Stock createStock(Stock stock);
+    Stock updateStock(Long id,Stock stock);
+    Stock getStockById(Long id);
+    List <Stock> getAllStocks();
+    void deactivateStock(Long id);
+
 
 }
---
+++ src/main/java/com/example/demo/service/UserPortfolioService.java
@@ -0,0 +1,14 @@
+import java.util.*;
+package com.example.demo.service;
+package com.example.demo.model.UserPortfolio;
+
+public interface StockService extends Stock
+{
+    Stock createStock(Stock stock);
+    Stock updateStock(Long id,Stock stock);
+    Stock getStockById(Long id);
+    List <Stock> getAllStocks();
+    void deactivateStock(Long id);
+
+
+}
