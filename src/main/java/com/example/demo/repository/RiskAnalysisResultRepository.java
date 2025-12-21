629c680103eb862a8980c56224cd97d507ae985c
bala-amypo <bala@amypo.in>
2025-12-21 03:38:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
+++ src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.RiskAnalysisResult;
 import java.util.List;
+
 public interface RiskAnalysisResultRepository extends JpaRepository<RiskAnalysisResult, Long> {
     List<RiskAnalysisResult> findByPortfolioId(Long portfolioId);  // Add custom query
 }
--- src/main/java/com/example/demo/repository/StockRepository.java
+++ src/main/java/com/example/demo/repository/StockRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.Stock;
 
-@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    Stock findByTicker(String ticker);
 }
