5ea10a687b8012d415e27117095f1a3f406a7824
bala-amypo <bala@amypo.in>
2025-12-24 16:09:03 +0000

    User commit

--- src/main/java/com/example/demo/service/PortfolioHoldingService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.PortfolioHolding;
-import java.util.List;
-
-public interface PortfolioHoldingService {
-    List<PortfolioHolding> getHoldingsByPortfolio(Long portfolioId);
-}
